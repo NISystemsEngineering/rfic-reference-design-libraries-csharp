c69568eebd821f7c55b992e38eb18d90255ea225
mdwhitten <47825680+mdwhitten@users.noreply.github.com>
2019-02-28 09:18:55 -0600

    Update GetAssemblyVersion.ps1
    Attempting to update the variable **and build number** in the PS script

--- dotNET/Build Tools/GetAssemblyVersion.ps1
+++ dotNET/Build Tools/GetAssemblyVersion.ps1
@@ -2,4 +2,6 @@
     [Parameter(Mandatory=$true)][string]$path
  )
 $result = Select-String -Path $path -Pattern 'AssemblyFileVersion\(\"(.*)\"\)'
-$result.Matches.Groups[1].Value
+$version = $result.Matches.Groups[1].Value
+Write-Host "##vso[task.setvariable variable=assemblyVersion]$($version)"
+Write-Host "##vso[build.updatebuildnumber]$version"
