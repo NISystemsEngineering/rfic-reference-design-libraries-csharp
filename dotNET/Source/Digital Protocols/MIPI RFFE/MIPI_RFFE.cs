f076ff8d4f9482abb57536cd56d705941b9c2f8b
mdwhitten <47825680+mdwhitten@users.noreply.github.com>
2019-03-11 17:57:53 -0500

    Created RFFE MIPI Project
    Initial project exploration

---
+++ dotNET/Source/Digital Protocols/MIPI RFFE/MIPI_RFFE.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NationalInstruments.ModularInstruments.NIDigital;
+
+namespace NationalInstruments.ReferenceDesignLibraries.DigitalProtocols
+{
+    public class MIPI_RFFE
+    {
+        #region Type Definitions
+        public struct RegisterData
+        {
+            public byte SlaveAddress;
+            public ushort RegisterAddress;
+            public byte[] WriteRegisterData;
+            public byte ByteCount;
+        }
+        public struct ReadData
+        {
+            public string[] Register;
+            public byte[] Data;
+        }
+        #endregion
+        #region Command Classes
+        public abstract class RFFECommand
+        {
+            public abstract int ByteCountLimit
+            {
+                get;
+            }
+            public abstract int UpperAddressLimit
+            {
+                get;
+            }
+            static public RFFECommand Reg0Write
+            {
+                get { return new Reg0Write(); }
+            }
+
+            public void ValidateLogic(RegisterData regData)
+            {
+                if (regData.SlaveAddress < 0x0 || regData.SlaveAddress > 0x10)
+                {
+                    throw new System.ArgumentOutOfRangeException("SlaveAddress", regData.SlaveAddress.ToString("X"), "Slave Address out of range");
+                }
+                if (regData.RegisterAddress < 0x0 || regData.RegisterAddress > UpperAddressLimit )
+                {
+                    throw new System.ArgumentOutOfRangeException("RegisterAddress", regData.SlaveAddress.ToString("X"), 
+                        "Register Address out of range. Check that the address is valid based on the selected command.");
+                }
+                if (regData.ByteCount <= 0 || regData.ByteCount > ByteCountLimit)
+                {
+                    throw new System.ArgumentOutOfRangeException("ByteCount", regData.ByteCount, "Byte Count out of range");
+                }
+            }
+
+        }
+        public class Reg0Write : RFFECommand
+        {
+            public override int ByteCountLimit => 1;
+            public override int UpperAddressLimit => 0xFFFF;
+        }
+        #endregion
+        public static ReadData BurstRFFE(NIDigital niDigital, RegisterData regData, string pinName, RFFECommand rffeCommand)
+        {
+            rffeCommand.ValidateLogic(regData);
+            return new ReadData();
+        }
+    }
+}
---
+++ dotNET/Source/Digital Protocols/MIPI RFFE/Multiple Command Programmatic Burst.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using NationalInstruments.ModularInstruments.NIDigital;
+using static NationalInstruments.ReferenceDesignLibraries.Digital;
+using static NationalInstruments.ReferenceDesignLibraries.DigitalProtocols.MIPI_RFFE;
+
+namespace MIPI_RFFE
+{
+    class Multiple_Command_Programmatic_Burst
+    {
+        public static void Main()
+        {
+            NIDigital digital = new NIDigital("PXI1Slot2", false, false, "");
+
+            string pinMapPath = Path.GetFullPath(@"Support Files\PinMap.pinmap");
+            string specPath = Path.GetFullPath(@"Support Files\Specifications.specs");
+            string levelsPath = Path.GetFullPath(@"Support Files\PinLevels.digilevels");
+            string timingPath = Path.GetFullPath(@"Support Files\Timing.digitiming");
+            string patternPath = Path.GetFullPath(@"Support Files\Pattern.digipat");
+
+            ProjectFiles projectFiles = new ProjectFiles
+            {
+                PinMapFile = pinMapPath,
+                SpecificationsFiles = new string[1] { specPath },
+                PinLevelsFiles = new string[1] { levelsPath },
+                TimingFiles = new string[1] { timingPath },
+                DigitalPatternFiles = new string[1] { patternPath }
+            };
+
+            BurstRFFE(digital, new RegisterData(), "", RFFECommand.Reg0Write);
+    }
+
+    }
+}
