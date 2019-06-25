8267f7ab6523a0d7318a4acdda69bf2e31d7413f
mdwhitten <47825680+mdwhitten@users.noreply.github.com>
2019-06-25 14:48:44 -0500

    Added build tools for validating code conformity and sequence quality
    - "Run Conformity Checks" PowerShell script will validate that the code meets some requirements laid out in CONTRIBUTING.md
    - "Automation Sequence Analysis" will run various tests on the TestStand sequences to ensure that they are valid

---
+++ build-tools/MatchFileText.ps1
@@ -0,0 +1,97 @@
+[CmdletBinding()]
+
+param (
+    [string]$FileOrFolderPath, #Defines the file or folder path to find files in
+    [string]$SearchTextOrPattern, #Defines the desired search text to find or pattern to match in a file or files
+    [string]$FilePattern = "*.*", #Defines the file pattern to search for if a folder is specified
+    [string[]]$ExcludeFilePattern = "", #Defines files or paths to exclude from searching
+    [string[]]$ValidMatchValues = "",
+    [switch]$RecurseDirectory = $false,
+    [switch]$IgnoreFilesWithoutMatch = $false
+)
+
+#Determine if the input path is a file or folder
+$IsFolder = (Get-Item $FileOrFolderPath) -is [System.IO.DirectoryInfo]
+
+#If folder, we need to get the sub-items
+if ($IsFolder) {
+    #Recurse the directory and subdirectories to get all matching files 
+    if ($RecurseDirectory) {
+        $files = Get-ChildItem -File -Filter $FilePattern -Path $FileOrFolderPath -Recurse -Exclude $ExcludeFilePattern
+    }
+    #Search the directory to get matching files
+    else {
+        $files = Get-ChildItem -File -Filter $FilePattern -Path $FileOrFolderPath -Exclude $ExcludeFilePattern
+    }
+    #We are using relative paths later, so set the working directory to the current top-level folder
+    Push-Location $FileOrFolderPath
+}
+#Not folder, so just get the file object
+else {
+    $files = Get-Item $FileOrFolderPath
+    #We are using relative paths later, so set the working directory to the current top-level folder
+    Push-Location $files[0].Directory
+}
+
+#Ensure that one or more files are found
+if ($files.Count -gt 0) {
+    #In some cases the developer may not want to error if the pattern is not found;
+    #the developer may only care to check the value if it exists and ortherwise ignore it
+    if (!$IgnoreFilesWithoutMatch)
+    {
+        Foreach ($file in $files) {
+            
+            #Use Get-Content to get full file text as a single string for multi-line matching
+            #Use the Select-String cmdlet to search the file text for the search patttern
+            #Quiet indicates it will simply return a boolean value for each file if a match is found
+            $simpleResult = $file | Get-Content -Raw | Select-String -Pattern $SearchTextOrPattern -Quiet
+
+            #Relative paths are much easier to understand since this is a part of a build process
+            $relativePath = Resolve-Path -Relative $file.FullName
+
+            #Check to see if a match was found; if not, throw an error
+            if (!$simpleResult -or $simpleResult -eq $null) {
+                Write-Error "File ""$relativePath"" does not include any match for the requested search string." -Category InvalidData
+            }
+            else {
+                #Only visible if the script is called with the -Verbose arguement
+                Write-Verbose "File ""$relativePath"" includes one or more matches for the requested search string."
+            }
+        }
+    }
+
+    #Only try to match specific text if the expected value is not null or empty
+    if (![string]::IsNullOrEmpty($ValidMatchValues)) {
+
+        Foreach ($file in $files) {
+            
+            #Use Get-Content to get full file text as a single string for multi-line matching
+            $results = $file | Get-Content -Raw | Select-String -Pattern $SearchTextOrPattern -AllMatches
+
+            Foreach ($result in $results) {
+                #Relative paths are much easier to understand since this is a part of a build process
+                $relativePath = Resolve-Path -Relative $file.FullName
+
+                #Retrieve the captured text which is contained in Group 1, Group 0 being the whole match
+                $matchedValue = $result.Matches.Groups[1].Value
+
+                $testStrings = Compare-Object -ReferenceObject $ValidMatchValues -DifferenceObject $matchedValue -IncludeEqual -ExcludeDifferent -PassThru
+
+                if ($testStrings.Count -le 0) {
+                    Write-Error "File ""$relativePath"" has value of ""$matchedValue"" instead of ""$($ValidMatchValues)""."
+                }
+                else {
+                    #Only visible if the script is called with the -Verbose arguement
+                    Write-Verbose "File ""$relativePath"" matches the expected value of ""$ValidMatchValues""."
+                }
+            }
+        }
+    }
+    Write-Host "A total of $($files.Count) files were successfully searched."
+}
+else {
+    Write-Warning "No files were found matching the input parameters."
+}
+
+#Return the current directory to where it was before
+Pop-Location
