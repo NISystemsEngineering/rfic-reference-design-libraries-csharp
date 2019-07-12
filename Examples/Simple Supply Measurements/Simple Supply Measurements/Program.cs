03b2a5f971d773e38038d1c9fc1a38e923fcb5f0
mdwhitten <47825680+mdwhitten@users.noreply.github.com>
2019-07-12 12:48:50 -0500

    Moved Supply example to the proper folder

--- Examples/Simple Supply Measurements/Simple Supply Measurements/Program.cs
+++ Examples/Simple Supply Measurements/Simple Supply Measurements/Program.cs
@@ -14,21 +14,25 @@ namespace NationalInstruments.ReferenceDesignLibraries.Examples
             string channelNames = "0";
             NIDCPower dcPower = new NIDCPower("4139", channelNames, false);
 
+            // Configure instrument settings
             SupplyConfiguration supplyConfig = new SupplyConfiguration();
             supplyConfig.SetDefaults();
 
             supplyConfig.OutputFunction = DCPowerSourceOutputFunction.DCVoltage;
             supplyConfig.VoltageLevel_V = 3;
             supplyConfig.CurrentLevel_A = 1;
-            supplyConfig.TransientResponseMode = DCPowerSourceTransientResponse.Fast;
 
             ConfigureSupply(dcPower, supplyConfig, channelNames);
 
+            // Configure measurement related parameters
             MeasurementConfiguration measConfig = new MeasurementConfiguration
             {
                 MeasureWhenMode = DCPowerMeasurementWhen.AutomaticallyAfterSourceComplete,
                 SenseMode = DCPowerMeasurementSense.Remote,
-                MeasurementMode = MeasurementConfiguration.MeasurementModeConfiguration.SinglePoint,
+                // A MeasurementMode of "Record" acquires multiple smaples over the requested measurement 
+                // time at the supply's maximum sampling rate. "Single Point" will take a single measurement
+                // over that duration and average the power and current results.
+                MeasurementMode = MeasurementConfiguration.MeasurementModeConfiguration.Record,
                 MeasurementTime_s = 2e-3,
                 MeasurementTriggerTerminal = "PXI_Trig0"
             };
@@ -38,7 +42,14 @@ namespace NationalInstruments.ReferenceDesignLibraries.Examples
             TurnSupplyOnOrOff(dcPower, SupplyPowerMode.PowerOn, channelNames);
 
             MeasurementResults results = MeasureSupplyIV(dcPower, channelNames);
-            results = Utilities.CalculateAverageIV(results, 3);
+
+            // Calculate the average of the acquired results
+            results = Utilities.CalculateAverageIV(results);
+
+            Console.WriteLine($"The average voltage measured was {results.Voltage_V[0]} with a current of {results.Current_A[0]}");
+            Console.WriteLine("Press any key to turn off the supply and exit the program.");
+
+            Console.ReadKey();
 
             TurnSupplyOnOrOff(dcPower, SupplyPowerMode.PowerOff, channelNames);
 
