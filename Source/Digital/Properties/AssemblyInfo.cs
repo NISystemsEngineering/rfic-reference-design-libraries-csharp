d43d35cd3518c97fb89436c8cbe121e52bea8ecf
mdwhitten <47825680+mdwhitten@users.noreply.github.com>
2019-07-15 14:31:20 -0500

    Updated all version numbers of modules to reflect changes to each module and overall project

--- Source/Digital/Properties/AssemblyInfo.cs
+++ Source/Digital/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Digital")]
 
-[assembly: AssemblyFileVersion("1.3.3.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
