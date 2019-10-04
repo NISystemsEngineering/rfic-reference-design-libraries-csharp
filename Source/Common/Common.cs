5ad3e9ae9ce0a13477eb033de0cf2282c22a5cab
Sean Moore <sean.moore@ni.com>
2019-10-04 15:25:29 -0500

    apply new lo config types to rfmx instr

---
+++ Source/Common/Common.cs
@@ -0,0 +1,37 @@
+﻿namespace NationalInstruments.ReferenceDesignLibraries
+{
+    public enum LocalOscillatorSharingMode
+    {
+        None,
+        Automatic,
+        Manual
+    }
+
+    public enum LocalOscillatorOffsetMode
+    {
+        NoOffset,
+        Automatic,
+        UserDefined
+    }
+
+    public struct LocalOscillatorConfiguration
+    {
+        public string ChannelName;
+        public LocalOscillatorSharingMode SharingMode;
+        public string Source;
+        public LocalOscillatorOffsetMode OffsetMode;
+        public double Offset_Hz;
+
+        public static LocalOscillatorConfiguration GetDefault()
+        {
+            return new LocalOscillatorConfiguration()
+            {
+                ChannelName = "",
+                SharingMode = LocalOscillatorSharingMode.Automatic,
+                Source = "Onboard", // since default sharing style is automatic, the initial default value here doesn't matter much
+                OffsetMode = LocalOscillatorOffsetMode.Automatic,
+                Offset_Hz = 0.0
+            };
+        }
+    }
+}
--- Source/SA/RFmx Instr/RFmxInstr.cs
+++ Source/SA/RFmx Instr/RFmxInstr.cs
@@ -1,66 +1,78 @@
 ﻿using NationalInstruments.RFmx.InstrMX;
 
-
 namespace NationalInstruments.ReferenceDesignLibraries.SA
 {
     public static class RFmxInstr
     {
         #region Type Definitions
-        public struct LoConfiguration
+        public struct InstrumentConfiguration
         {
-            public string LoSharingMode; //{"None","Automatic","Manual"}
-            public string LoSource;
-            public string LoOffsetMode; //{"NoOffset","Automatic","UserDefined"}
-            public double LoOffset_Hz;
+            public LocalOscillatorConfiguration[] LoConfigurations;
 
-            public static LoConfiguration GetDefault()
+            public static InstrumentConfiguration GetDefault()
             {
-                return new LoConfiguration
+                return new InstrumentConfiguration
                 {
-                    LoSharingMode = "Automatic",
-                    LoSource = RFmxInstrMXConstants.LOSourceLOIn,
-                    LoOffsetMode = "Automatic",
-                    LoOffset_Hz     = 0                                   
+                    LoConfigurations = new LocalOscillatorConfiguration[] { LocalOscillatorConfiguration.GetDefault() }       
                 };
             }
-        }
 
-        #endregion
-        #region Instrument Configurations
-        public static void ConfigureInstrument(RFmxInstrMX sessionHandle, LoConfiguration loConfig, string selectorString = "")
-        {
-            switch (loConfig.LoSharingMode.ToLower())
+            public static InstrumentConfiguration GetDefault(RFmxInstrMX handle)
             {
-
-                default:
-                    sessionHandle.SetAutomaticSGSASharedLO(selectorString, RFmxInstrMXAutomaticSGSASharedLO.Disabled);
-                    sessionHandle.SetLOSource("", RFmxInstrMXConstants.LOSourceOnboard);
-                    break;
-                case "automatic":
-                    sessionHandle.SetAutomaticSGSASharedLO(selectorString, RFmxInstrMXAutomaticSGSASharedLO.Enabled);
-                    sessionHandle.ResetAttribute("", RFmxInstrMXPropertyId.LOSource);
-                    break;
-                case "manual":
-                    sessionHandle.SetAutomaticSGSASharedLO(selectorString, RFmxInstrMXAutomaticSGSASharedLO.Disabled);
-                    sessionHandle.SetLOSource(selectorString, loConfig.LoSource);
-                    break;
-                
+                InstrumentConfiguration instrConfig = GetDefault();
+                handle.GetInstrumentModel("", out string model);
+                switch (model)
+                {
+                    case "NI PXIe-5830":
+                    case "NI PXIe-5831":
+                        LocalOscillatorConfiguration lo1Config = LocalOscillatorConfiguration.GetDefault();
+                        lo1Config.ChannelName = "LO1";
+                        LocalOscillatorConfiguration lo2Config = LocalOscillatorConfiguration.GetDefault();
+                        lo2Config.ChannelName = "LO2";
+                        instrConfig.LoConfigurations = new LocalOscillatorConfiguration[] { lo1Config, lo2Config };
+                        break;
+                }
+                return instrConfig;
             }
+        }
+        #endregion
 
-            switch (loConfig.LoOffsetMode.ToLower())
+        #region Instrument Configurations
+        public static void ConfigureInstrument(RFmxInstrMX instrHandle, InstrumentConfiguration instrConfig)
+        {
+            foreach (LocalOscillatorConfiguration loConfig in instrConfig.LoConfigurations)
             {
-                case "nooffset": default:
-                    sessionHandle.SetLOLeakageAvoidanceEnabled(selectorString,RFmxInstrMXLOLeakageAvoidanceEnabled.False);
-                    sessionHandle.SetDownconverterFrequencyOffset(selectorString, 0);
-                    break;
-                case "automatic":
-                    sessionHandle.SetLOLeakageAvoidanceEnabled(selectorString, RFmxInstrMXLOLeakageAvoidanceEnabled.True);
-                    sessionHandle.ResetAttribute(selectorString, RFmxInstrMXPropertyId.DownconverterFrequencyOffset);
-                    break;
-                case "userdefined":
-                    sessionHandle.SetLOLeakageAvoidanceEnabled(selectorString, RFmxInstrMXLOLeakageAvoidanceEnabled.False);
-                    sessionHandle.SetDownconverterFrequencyOffset(selectorString, loConfig.LoOffset_Hz);
-                    break;
+                switch (loConfig.SharingMode)
+                {
+                    case LocalOscillatorSharingMode.None:
+                        instrHandle.SetAutomaticSGSASharedLO(loConfig.ChannelName, RFmxInstrMXAutomaticSGSASharedLO.Disabled);
+                        instrHandle.SetLOSource(loConfig.ChannelName, RFmxInstrMXConstants.LOSourceOnboard);
+                        break;
+                    case LocalOscillatorSharingMode.Manual:
+                        instrHandle.SetAutomaticSGSASharedLO(loConfig.ChannelName, RFmxInstrMXAutomaticSGSASharedLO.Disabled);
+                        instrHandle.SetLOSource(loConfig.ChannelName, loConfig.Source);
+                        break;
+                    default: // default to automatic case
+                        instrHandle.SetAutomaticSGSASharedLO(loConfig.ChannelName, RFmxInstrMXAutomaticSGSASharedLO.Enabled);
+                        instrHandle.ResetAttribute(loConfig.ChannelName, RFmxInstrMXPropertyId.LOSource);
+                        break;
+                }
+
+                switch (loConfig.OffsetMode)
+                {
+                    case LocalOscillatorOffsetMode.NoOffset:
+                        instrHandle.SetLOLeakageAvoidanceEnabled(loConfig.ChannelName, RFmxInstrMXLOLeakageAvoidanceEnabled.False);
+                        instrHandle.SetDownconverterFrequencyOffset(loConfig.ChannelName, 0.0);
+                        break;
+                    case LocalOscillatorOffsetMode.UserDefined:
+                        instrHandle.SetLOLeakageAvoidanceEnabled(loConfig.ChannelName, RFmxInstrMXLOLeakageAvoidanceEnabled.False);
+                        instrHandle.SetDownconverterFrequencyOffset(loConfig.ChannelName, loConfig.Offset_Hz);
+                        break;
+                    default:
+                        instrHandle.SetLOLeakageAvoidanceEnabled(loConfig.ChannelName, RFmxInstrMXLOLeakageAvoidanceEnabled.True);
+                        instrHandle.ResetAttribute(loConfig.ChannelName, RFmxInstrMXPropertyId.DownconverterFrequencyOffset);
+                        break;
+                }
             }
         }
         #endregion
