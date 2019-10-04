1a39ac91e37564a45e947eaf7cd3f5dcbcfa28c2
Sean Moore <sean.moore@ni.com>
2019-10-04 13:59:58 -0500

    add common project for common types

---
+++ Source/Common/Enumerations.cs
@@ -0,0 +1,16 @@
+﻿namespace NationalInstruments.ReferenceDesignLibraries
+{
+    public enum LoSharingMode
+    {
+        None,
+        Automatic,
+        Manual
+    }
+
+    public enum LoOffsetMode
+    {
+        NoOffset,
+        Automatic,
+        UserDefined
+    }
+}
