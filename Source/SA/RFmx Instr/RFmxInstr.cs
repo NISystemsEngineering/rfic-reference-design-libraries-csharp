8aa27a05debec85c1d6075205f0c3c35a536caa6
Sean Moore <sean.moore@ni.com>
2019-10-30 12:43:50 -0500

    add rfmx instr project

---
+++ Source/SA/RFmx Instr/RFmxInstr.cs
@@ -0,0 +1,38 @@
+﻿using NationalInstruments.RFmx.InstrMX;
+
+namespace NationalInstruments.ReferenceDesignLibraries.SA
+{
+    public static class RFmxInstr
+    {
+        #region Type Definitions
+        public struct InstrumentConfiguration
+        {
+            public LocalOscillatorSharingMode LOSharingMode;
+            public LocalOscillatorFrequencyOffsetMode LOOffsetMode;
+
+            public static InstrumentConfiguration GetDefault()
+            {
+                return new InstrumentConfiguration
+                {
+                    LOSharingMode = LocalOscillatorSharingMode.Automatic,
+                };
+            }
+        }
+        #endregion
+
+        #region Instrument Configurations
+        public static void ConfigureInstrument(RFmxInstrMX instrHandle, InstrumentConfiguration instrConfig)
+        {
+            if (instrConfig.LOSharingMode == LocalOscillatorSharingMode.None)
+                instrHandle.ConfigureAutomaticSGSASharedLO("", RFmxInstrMXAutomaticSGSASharedLO.Disabled);
+            else
+                instrHandle.ConfigureAutomaticSGSASharedLO("", RFmxInstrMXAutomaticSGSASharedLO.Enabled);
+
+            if (instrConfig.LOOffsetMode == LocalOscillatorFrequencyOffsetMode.NoOffset)
+                instrHandle.SetLOLeakageAvoidanceEnabled("", RFmxInstrMXLOLeakageAvoidanceEnabled.False);
+            else
+                instrHandle.ResetAttribute("", RFmxInstrMXPropertyId.LOLeakageAvoidanceEnabled);
+        }
+        #endregion
+    }
+}
