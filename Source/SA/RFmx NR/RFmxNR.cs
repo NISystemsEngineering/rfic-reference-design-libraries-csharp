67b13ff96e34d4693e9f5b849cc0be56c9c1788b
Michael Whitten <michael.whitten@ni.com>
2019-12-10 15:29:12 -0600

    Completed documentation of NR types

--- Source/SA/RFmx NR/RFmxNR.cs
+++ Source/SA/RFmx NR/RFmxNR.cs
@@ -9,19 +9,38 @@ namespace NationalInstruments.ReferenceDesignLibraries.SA
         /// <summary>Defines common settings for a single component carrier.</summary>
         public struct ComponentCarrierConfiguration
         {
+            /// <summary>Specifies the channel bandwidth of the signal being measured. This value is expressed in Hz. See the RFmx help for more documention of this parameter.</summary>
             public double Bandwidth_Hz;
+            /// <summary>Specifies a physical layer cell identity. See the RFmx help for more documention of this parameter.</summary>
             public int CellId;
+            /// <summary>Specifies the modulation scheme used in the physical uplink shared channel (PUSCH) of the signal being measured. 
+            /// See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXPuschModulationType PuschModulationType;
+            /// <summary>Specifies the subcarrier spacing of the bandwidth part used in the component carrier. See the RFmx help for more documention of this parameter.</summary>
             public double SubcarrierSpacing_Hz;
+            /// <summary>Specifies the starting resource block number of a PUSCH cluster. This property is ignored if you set 
+            /// <see cref="StandardConfiguration.AutoResourceBlockDetectionEnabled"/> to True. See the RFmx help for more documention of this parameter.</summary>
             public int PuschResourceBlockOffset;
+            /// <summary>Specifies the number of consecutive resource blocks in a physical uplink shared channel (PUSCH) cluster. This property is ignored if you set 
+            /// <see cref="StandardConfiguration.AutoResourceBlockDetectionEnabled"/> to True. See the RFmx help for more documention of this parameter.</summary>
             public int PuschNumberOfResourceBlocks;
+            /// <summary>Specifies whether transform precoding is enabled. Enable transform precoding when analyzing a DFT-s-OFDM waveform.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXPuschTransformPrecodingEnabled PuschTransformPrecodingEnabled;
+            /// <summary>Specifies the slot allocation in NR Frame. This defines the indices of the allocated slots. See the RFmx help for more documention of this parameter.</summary>
             public string PuschSlotAllocation;
+            /// <summary>Specifies the symbol allocation of each slot allocation. See the RFmx help for more documention of this parameter.</summary>
             public string PuschSymbolAllocation;
+            /// <summary>Specifies the configuration type of DMRS. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXPuschDmrsConfigurationType PuschDmrsConfigurationType;
+            /// <summary>Specifies the mapping type of DMRS. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXPuschMappingType PuschMappingType;
+            /// <summary>Specifies the position of first DMRS symbol in a slot when you set <see cref="PuschDmrsConfigurationType"/> to Type A.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public int PuschDmrsTypeAPosition;
+            /// <summary>Specifies whether the DMRS is single-symbol or double-symbol. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXPuschDmrsDuration PuschDmrsDuration;
+            /// <summary>Specifies the number of additional sets of consecutive DMRS symbols in a slot. See the RFmx help for more documention of this parameter.</summary>
             public int PuschDmrsAdditionalPositions;
 
             /// <summary>Returns the struct with default values set.</summary>
@@ -51,12 +70,23 @@ namespace NationalInstruments.ReferenceDesignLibraries.SA
         /// <summary>Defines common settings related to the standard of the measured NR signal.</summary>
         public struct StandardConfiguration
         {
+            /// <summary>Specifies the link direction of the received signal. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXLinkDirection LinkDirection;
+            /// <summary>Specifies whether to use channel bandwidth and subcarrier spacing configuration supported in the frequency range 1 (sub 6 GHz)
+            /// or the frequency range 2 (above 24 GHz). See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXFrequencyRange FrequencyRange;
+            /// <summary>Specifies the evolved universal terrestrial radio access (E-UTRA) or NR operating frequency band of a subblock.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public int Band;
+            /// <summary>Specifies whether the values of modulation type, number of resource block clusters, resource block offsets, and number of resource blocks are
+            /// auto-detected by the measurement or configured by you. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXAutoResourceBlockDetectionEnabled AutoResourceBlockDetectionEnabled;
+            /// <summary>Specifies the NR test model type when you set <see cref="LinkDirection"/> to Downlink. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXDownlinkTestModel DownlinkTestModel;
+            /// <summary>Specifies the duplexing technique of the signal being measured when you set <see cref="LinkDirection"/> to Downlink.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXDownlinkTestModelDuplexScheme DownlinkTestModelDuplexScheme;
+            /// <summary>Specifies an array of common configurations for each component carrier in the signal.</summary>
             public ComponentCarrierConfiguration[] ComponentCarrierConfigurations;
 
             /// <summary>Returns the struct with default values set.</summary>
@@ -79,11 +109,18 @@ namespace NationalInstruments.ReferenceDesignLibraries.SA
         /// <summary>Defines common settings for the ModAcc measurement.</summary>
         public struct ModAccConfiguration
         {
+            /// <summary>Specifies the units in which measurement offset and measurement length are specified. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXModAccMeasurementLengthUnit MeasurementLengthUnit;
+            /// <summary>Specifies the measurement offset to skip from the synchronization boundary. See the RFmx help for more documention of this parameter.</summary>
             public double MeasurementOffset;
+            /// <summary>Specifies the measurement length in units specified by <see cref="MeasurementLengthUnit"/>. See the RFmx help for more documention of this parameter.</summary>
             public double MeasurementLength;
+            /// <summary>Specifies the units of the EVM results. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXModAccEvmUnit EvmUnit;
+            /// <summary>Enables averaging for the measurement. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXModAccAveragingEnabled AveragingEnabled;
+            /// <summary>Specifies the number of acquisitions used for averaging when you set <see cref="AveragingEnabled"/> to True.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public int AveragingCount;
 
             /// <summary>Returns the struct with default values set.</summary>
@@ -105,33 +142,58 @@ namespace NationalInstruments.ReferenceDesignLibraries.SA
         /// <summary>Defines common results of the ModAcc measurement for a single component carrier.</summary>
         public struct ModAccComponentCarrierResults
         {
+            /// <summary>Returns the subcarrier index where the max peak composite EVM occurs. See the RFmx help for more documention of this parameter.</summary>
             public int PeakCompositeEvmSubcarrierIndex;
+            /// <summary>Returns the symbol index where the max peak composite EVM occurs. See the RFmx help for more documention of this parameter.</summary>
             public int PeakCompositeEvmSymbolIndex;
-            public double MeanRmsCompositeEvm;
-            public double MaxPeakCompositeEvm;
-            public double MeanFrequencyError_Hz;
+            /// <summary>Returns the slot index where the max peak composite EVM occurs. See the RFmx help for more documention of this parameter.</summary>
             public int PeakCompositeEvmSlotIndex;
+            /// <summary>Returns the mean value of RMS EVMs calculated over measurement length. See the RFmx help for more documention of this parameter.</summary>
+            public double MeanRmsCompositeEvm;
+            /// <summary>Returns the maximum value of peak EVMs calculated over measurement length. See the RFmx help for more documention of this parameter.</summary>
+            public double MaxPeakCompositeEvm;
+            /// <summary>Returns the estimated carrier frequency offset averaged over measurement length. This value is expressed in Hz.
+            /// See the RFmx help for more documention of this parameter.</summary>
+            public double MeanFrequencyError_Hz;
         }
 
         /// <summary>Defines common results of the ModAcc measurement.</summary>
         public struct ModAccResults
         {
+            /// <summary>Returns ModAcc results for each configured component carrier in the signal.</summary>
             public ModAccComponentCarrierResults[] ComponentCarrierResults;
         }
 
         /// <summary>Defines common settings for the ACP measurement.</summary>
         public struct AcpConfiguration
         {
+            /// <summary>Specifies the number of universal terrestrial radio access (UTRA) adjacent channel offsets to be configured at offset positions.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public int NumberOfUtraOffsets;
+            /// <summary>Specifies the number of evolved universal terrestrial radio access (E-UTRA) adjacent channel offsets to be configured at offset positions.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public int NumberOfEutraOffsets;
+            /// <summary>Specifies the number of NR adjacent channel offsets to be configured at offset positions. See the RFmx help for more documention of this parameter.</summary>
             public int NumberOfNrOffsets;
+            /// <summary>Specifies the number of ENDC adjacent channel offsets to be configured at offset positions. See the RFmx help for more documention of this parameter.</summary>
             public int NumberOfEndcOffsets;
+            /// <summary>Specifies the method for performing the ACP measurement. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXAcpMeasurementMethod MeasurementMethod;
+            /// <summary>Specifies whether to enable compensation of the channel powers for the inherent noise floor of the signal analyzer.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXAcpNoiseCompensationEnabled NoiseCompensationEnabled;
+            /// <summary>Specifies whether the measurement sets the sweep time. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXAcpSweepTimeAuto SweepTimeAuto;
+            /// <summary>Specifies the sweep time when you set <see cref="SweepTimeAuto"/> to False. This value is expressed in seconds.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public double SweepTimeInterval_s;
+            /// <summary>Specifies whether to enable averaging for the ACP measurement. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXAcpAveragingEnabled AveragingEnabled;
+            /// <summary>Specifies the number of acquisitions used for averaging when you set <see cref="AveragingEnabled"/> to True.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public int AveragingCount;
+            /// <summary>Specifies the averaging type for averaging multiple spectrum acquisitions. The averaged spectrum is used for ACP measurement.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXAcpAveragingType AveragingType;
 
             /// <summary>Returns the struct with default values set.</summary>
@@ -158,35 +220,56 @@ namespace NationalInstruments.ReferenceDesignLibraries.SA
         /// <summary>Defines common results of the ACP measurement for a single defined offset.</summary>
         public struct AcpOffsetResults
         {
+            /// <summary>Returns the lower (negative) offset channel power. This value is expressed in dBm. See the RFmx help for more documention of this parameter.</summary>
             public double LowerAbsolutePower_dBm;
+            /// <summary>Returns the power in lower (negative) offset channel relative to the total aggregated power. This value is expressed in dB. 
+            /// See the RFmx help for more documention of this parameter.</summary>
             public double LowerRelativePower_dB;
+            /// <summary>Returns the upper (positive) offset channel power. This value is expressed in dBm. See the RFmx help for more documention of this parameter.</summary>
             public double UpperAbsolutePower_dBm;
+            /// <summary>Returns the power in the upper (positive) offset channel relative to the total aggregated power. This value is expressed in dB.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public double UpperRelativePower_dB;
+            /// <summary>Returns the offset frequency of an offset channel. This value is expressed in Hz. See the RFmx help for more documention of this parameter.</summary>
             public double Frequency_Hz;
+            /// <summary>Returns the integration bandwidth of an offset channel. This value is expressed in Hz. See the RFmx help for more documention of this parameter.</summary>
             public double IntegrationBandwidth_Hz;
         }
 
         /// <summary>Defines common results of the ACP measurement for a single component carrier.</summary>
         public struct AcpComponentCarrierResults
         {
+            /// <summary>Returns the power measured over the integration bandwidth of the component carrier. This value is expressed in dBm.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public double AbsolutePower_dBm;
+            /// <summary>Returns the component carrier power relative to its subblock power. This value is expressed in dB. See the RFmx help for more documention of this parameter.</summary>
             public double RelativePower_dB;
         }
 
         /// <summary>Defines common results of the ACP measurement.</summary>
         public struct AcpResults
         {
+            /// <summary>Returns an array of ACP results for the configured offsets.</summary>
             public AcpOffsetResults[] OffsetResults;
+            /// <summary>Returns an array of ACP results for the component carriers of the signal.</summary>
             public AcpComponentCarrierResults[] ComponentCarrierResults;
         }
 
         /// <summary>Defines common settings for the CHP measurement.</summary>
         public struct ChpConfiguration
         {
+            /// <summary>Specifies whether the measurement sets the sweep time. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXChpSweepTimeAuto SweepTimeAuto;
+            /// <summary>Specifies the sweep time when you set <see cref="SweepTimeAuto"/> to False. This value is expressed in seconds.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public double SweepTimeInterval_s;
+            /// <summary>Specifies whether to enable averaging for the CHP measurement. See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXChpAveragingEnabled AveragingEnabled;
+            /// <summary>Specifies the number of acquisitions used for averaging when you set <see cref="AveragingEnabled"/> to True.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public int AveragingCount;
+            /// <summary>Specifies the averaging type for averaging multiple spectrum acquisitions. The averaged spectrum is used for CHP measurement.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public RFmxNRMXChpAveragingType AveragingType;
 
             /// <summary>Returns the struct with default values set.</summary>
@@ -207,14 +290,20 @@ namespace NationalInstruments.ReferenceDesignLibraries.SA
         /// <summary>Defines common results of the CHP measurement for a single component carrier.</summary>
         public struct ChpComponentCarrierResults
         {
+            /// <summary>Returns the power measured over the integration bandwidth of the component carrier. This value is expressed in dBm.
+            /// See the RFmx help for more documention of this parameter.</summary>
             public double AbsolutePower_dBm;
+            /// <summary>Returns the component carrier power relative to its subblock power. This value is expressed in dB. See the RFmx help for more documention of this parameter.</summary>
             public double RelativePower_dB;
         }
 
         /// <summary>Defines common results of the CHP measurement.</summary>
         public struct ChpResults
         {
+            /// <summary>Returns the total power of all the subblocks. This value is expressed in dBm. 
+            /// See the RFmx help for more documention of this parameter.</summary>
             public double TotalAggregatedPower_dBm;
+            /// <summary>Returns an array of CHP results for the component carriers in the signal.</summary>
             public ChpComponentCarrierResults[] ComponentCarrierResults;
         }
         #endregion
