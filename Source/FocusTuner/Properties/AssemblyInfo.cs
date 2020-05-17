58d74cfc4c3187aad890899c156020817c4b6663
hawang <hanrock.wang@ni.com>
2020-05-17 10:14:36 +0800

    use the shared SolutionInfo.cs

--- Source/FocusTuner/Properties/AssemblyInfo.cs
+++ Source/FocusTuner/Properties/AssemblyInfo.cs
@@ -8,12 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FocusTuner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FocusTuner")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
+[assembly: AssemblyFileVersion("1.0.0.0")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8d21d9a9-5420-4d00-95fe-f2de7cfbf157")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
